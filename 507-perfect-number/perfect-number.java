efd2ea5f1f2f13a6244fa213359b7b6f91385db3
Prince vasoya <136919187+vasoyaprince14@users.noreply.github.com>
2025-08-23 20:30:29 +0530

    Time: 2074 ms (62.87%) | Memory: 40.8 MB (19.98%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum = 0;
+
+        for (int i = 1;i<num;i++){
+            if (num % i == 0){
+                // System.out.println(i);
+                sum +=i;
+            }
+        }
+
+        return num == sum;
+        
+    }
+}
