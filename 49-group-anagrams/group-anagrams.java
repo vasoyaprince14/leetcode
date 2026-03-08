7b33daee7a29fef0382dc5626ef2335c344bcdd3
Prince vasoya <136919187+vasoyaprince14@users.noreply.github.com>
2026-03-08 10:13:07 +0530

    Time: 21 ms (11.60%) | Memory: 50.3 MB (18.76%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,31 +1,29 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
         if(strs.length == 0){
             return new ArrayList<>();
         }
+        Map<String , List> ans = new HashMap<>();
+        int count[] = new int [26];
 
-        Map<String ,List> ansMap = new HashMap<>();
-
-        int[] count = new int[26];
-
-        for(String str :strs){
+        for(String s : strs){
             Arrays.fill(count,0);
-            for(char c : str.toCharArray()){
-                count [c - 'a']++;
+            for(int i = 0 ; i<s.length();i++){
+                count[s.charAt(i)-'a']++;
             }
-
             StringBuilder sb = new StringBuilder("");
+
             for(int i : count){
-                sb.append('#');
+                sb.append("#");
                 sb.append(i);
             }
             String key = sb.toString();
-            if(!ansMap.containsKey(key)){
-                ansMap.put(key,new ArrayList<>());
+            if(!ans.containsKey(key)){
+                ans.put(key,new ArrayList<>());
             }
-            ansMap.get(key).add(str);
-        }
-        return new ArrayList(ansMap.values());
+            ans.get(key).add(s);
+        } 
+
+        return new ArrayList(ans.values());
     }
 }
